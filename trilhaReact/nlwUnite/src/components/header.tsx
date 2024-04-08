e609d266557bde34c6382b4c6027f5f498ca89d0
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2024-04-08 00:26:01 -0300

    update nlwunite ui completed

---
+++ trilhaReact/nlwUnite/src/components/header.tsx
@@ -0,0 +1,14 @@
+import nlwHeaderIcon from '../assets/nlwUnite-icon.svg';
+
+export function Header() {
+    return (
+        <div className='flex items-center gap-5'>
+            <img src={nlwHeaderIcon} alt="" />
+
+            <nav className='flex items-center gap-5 py-2'>
+                <a href="" className='font-medium text-sm text-zinc-300'>Eventos</a>
+                <a href="" className='font-medium text-sm'>Participantes</a>
+            </nav>
+        </div>
+    )
+}
